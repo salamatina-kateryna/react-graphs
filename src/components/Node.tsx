3d239cb374ba388b0df49a0b6321a703bfa2852a
ksalamatina <ksalamatina95@gmail.com>
2023-12-28 21:48:17 +0200

    Added styling.

--- src/components/Node.tsx
+++ src/components/Node.tsx
@@ -1,6 +1,11 @@
 import React, { useState } from "react";
 import { Handle, Position } from "react-flow-renderer";
-import { MultiSelect } from "react-multi-select-component";
+import Select, {
+  GetStyles,
+  GroupBase,
+  OptionsOrGroups,
+  components,
+} from "react-select";
 
 interface DefaultNodeProps {
   data: {
@@ -9,28 +14,134 @@ interface DefaultNodeProps {
   };
 }
 
-const options = [
-  { label: "Grapes 🍇", value: "grapes" },
-  { label: "Mango 🥭", value: "mango" },
-  { label: "Strawberry 🍓", value: "strawberry", disabled: true },
+const allOptions = [
+  { value: "option 1", label: "option 1" },
+  { value: "option 2", label: "option 2" },
+  { value: "option 3", label: "option 3" },
+  { value: "option 4", label: "option 4" },
 ];
 
-const DefaultNode: React.FC<DefaultNodeProps> = ({ data }) => {
-  const [selected, setSelected] = useState([]);
+interface IInputPropsOption {
+  getStyles: GetStyles<any, false, GroupBase<any>>;
+  isDisabled: boolean;
+  isFocused: boolean;
+  isSelected: boolean;
+  children: React.ReactNode;
+  innerProps: any;
+}
+
+const InputOption = ({
+  getStyles,
+  isDisabled,
+  isFocused,
+  isSelected,
+  children,
+  innerProps,
+  ...rest
+}: IInputPropsOption) => {
+  const [isActive, setIsActive] = useState(false);
+  const onMouseDown = () => {
+    console.log(123123);
+    setIsActive(true);
+  };
+  const onMouseUp = () => setIsActive(false);
+  const onMouseLeave = () => setIsActive(false);
+
+  // styles
+  let bg = "transparent";
+  if (isFocused) bg = "#eee";
+  if (isActive) bg = "#B2D4FF";
+
+  const style = {
+    alignItems: "center",
+    backgroundColor: bg,
+    color: "inherit",
+    display: "flex ",
+  };
+
+  // prop assignment
+  const props = {
+    ...innerProps,
+    onMouseDown,
+    onMouseUp,
+    onMouseLeave,
+    style,
+  };
 
   return (
-    <div className="react-flow__node-default">
+    // @ts-expect-error "Incorrect types passing"
+    <components.Option
+      {...rest}
+      isDisabled={isDisabled}
+      isFocused={isFocused}
+      isSelected={isSelected}
+      getStyles={getStyles}
+      innerProps={props}
+    >
+      <input type="checkbox" checked={isSelected} readOnly={true} />
+      {children}
+    </components.Option>
+  );
+};
+
+const DefaultNode: React.FC<DefaultNodeProps> = ({ data }) => {
+  const [selectedOptions, setSelectedOptions] = useState<
+    OptionsOrGroups<never, GroupBase<never>> | undefined
+  >([]);
+
+  const styleGraph = {
+    width: 235,
+    height: 118,
+    padding: "18px 3px 5px",
+    backgroundColor: "#D1E7DD",
+    borderRadius: 4,
+    border: "none",
+  };
+
+  const styleBlock = {
+    width: 235,
+    height: 72,
+    backgroundColor: "#ffffff",
+    marginBottom: 4,
+    borderRadius: 4,
+  };
+
+  return (
+    <div className="react-flow__node-default nodrag" style={styleGraph}>
       <Handle type="target" position={Position.Top} />
-      <MultiSelect
-        options={options}
-        value={selected}
-        onChange={setSelected}
-        labelledBy="Select"
+      <div style={styleBlock}></div>
+      <Select
+        defaultValue={[]}
+        closeMenuOnSelect={false}
+        hideSelectedOptions={false}
+        onChange={(options: any) => {
+          console.log(options);
+          if (Array.isArray(options)) {
+            setSelectedOptions(options.map((opt) => opt.value));
+          }
+        }}
+        options={allOptions}
+        components={{
+          Option: InputOption,
+        }}
+        placeholder={"Виберіть значення"}
+        styles={{
+          control: (baseStyles) => ({
+            ...baseStyles,
+            borderColor: "#479F76",
+          }),
+          placeholder: (baseStyles) => ({
+            ...baseStyles,
+            textAlign: "left",
+          }),
+          dropdownIndicator: (baseStyles) => ({
+            ...baseStyles,
+            svg: {
+              fill: "#2C7DFA",
+            },
+          }),
+        }}
       />
-      <div>
-        {data.label}
-        {/* Render any additional data properties as needed */}
-      </div>
       <Handle type="source" position={Position.Bottom} />
     </div>
   );
